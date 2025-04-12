93f4b1ac36d3b9416f94fb6cb605da4531816823
joaovolta <joaovolta4@gmail.com>
2025-04-11 21:36:40 -0300

    Projeto inicial: Criando a class Musica (Atributos e Metodos) e instanciando um objeto

---
+++ Musica.cs
@@ -0,0 +1,21 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibeFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duracao: {duracao}");
+        if(disponivel)
+        {
+            Console.WriteLine("Essa musica esta disponivel");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus+ para disponibilizar essa musica");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 =  new Musica();
+musica1.nome = "Eu seguirei";
+musica1.artista = "Frei Gilson";
+musica1.duracao = 207;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Let it be";
+musica2.artista = "Beatles";
+musica2.duracao = 160;
+musica2.disponivel = false;
+
+musica1.ExibeFichaTecnica();
+musica2.ExibeFichaTecnica();
