147f6c726e6fbbf831018334d8de6bd63842eea7
joaovolta <joaovolta4@gmail.com>
2025-04-15 12:22:31 -0300

    Adicao dos metodos de acesso (Public) utilizando o get e o set para não haver um acesso direto as variaveis

--- Musica.cs
+++ Musica.cs
@@ -1,16 +1,18 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    // A variavel utiliza =>, oque indica que a variavel esta somente para leitura (get)
+    public string DescricaoResumida => $"A musica {Nome} pertence a banda/artista {Artista}";
 
     public void ExibeFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duracao: {duracao}");
-        if(disponivel)
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duracao: {Duracao}");
+        if(Disponivel)
         {
             Console.WriteLine("Essa musica esta disponivel");
         } else
--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿Musica musica1 =  new Musica();
-musica1.nome = "Eu seguirei";
-musica1.artista = "Frei Gilson";
-musica1.duracao = 207;
-musica1.disponivel = true;
+musica1.Nome = "Eu seguirei";
+musica1.Artista = "Frei Gilson";
+musica1.Duracao = 207;
+musica1.Disponivel = true;
 
 Musica musica2 = new Musica();
-musica2.nome = "Let it be";
-musica2.artista = "Beatles";
-musica2.duracao = 160;
-musica2.disponivel = false;
+musica2.Nome = "Let it be";
+musica2.Artista = "Beatles";
+musica2.Duracao = 160;
+musica2.Disponivel = false;
 
 musica1.ExibeFichaTecnica();
 musica2.ExibeFichaTecnica();
